da363b4000e4fddc45dbdc3fb5e8feb80314dcdd
Kyle Harrison <kyle.harrison344@gmail.com>
2020-07-04 11:57:17 -0400

    front end mostly done

--- index.js
+++ index.js
@@ -21,4 +21,34 @@ $(document).ready(function () {
       },
     },
   });
+
+  //isotop filter
+  var $grid = $('.grid').isotope({
+    itemSelector: '.grid-item',
+    layoutMode: 'fitRows',
+  });
+
+  //fikter items on button click
+  $('.button-group').on('click', 'button', function () {
+    let filterValue = $(this).attr('data-filter');
+    $grid.isotope({ filter: filterValue });
+  });
+
+  //new cars carousel
+  $('#new-cars .owl-carousel').owlCarousel({
+    loop: true,
+    nav: false,
+    dots: true,
+    responsive: {
+      0: {
+        items: 1,
+      },
+      600: {
+        items: 3,
+      },
+      1000: {
+        items: 5,
+      },
+    },
+  });
 });
